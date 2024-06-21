855f50c5ed9098340477c5b09cc60372ba09c270
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2024-06-21 14:48:04 +0300

    ci/cd: update env

--- Presentation/ClientApp/src/environments/environment.ts
+++ Presentation/ClientApp/src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    apiUrl: 'https://zlahody.onrender.com/api',
+    chatHubUrl: 'https://zlahody.onrender.com/chat',
+    firebase: {
+        apiKey: "AIzaSyDtcD1shRD160neQ13zTJCwtGWHzZ9PAtg",
+        authDomain: "zlahody-1f7e9.firebaseapp.com",
+        projectId: "zlahody-1f7e9",
+        storageBucket: "zlahody-1f7e9.appspot.com",
+        messagingSenderId: "530536513996",
+        appId: "1:530536513996:web:becb9b3e8cb79ccf43dd69"
+    }
+};
